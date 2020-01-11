03116e3582e88010130826ff912d14856e2d8821
Emilio Araos <ea3dev@gmail.com>
2020-01-11 11:40:43 -0700

    if, elif, else statements

---
+++ control_flow.py
@@ -0,0 +1,35 @@
+if True:
+	print('Its true!')
+
+if 3 > 2:
+	print('Its true!!!')
+
+hungry = False
+
+if hungry:
+	print('Its very always TRUE!')
+else:
+	print('I am not Hungry!')
+
+
+loc = 'Auto Shop'
+if loc == 'Auto Shop':
+	print("Cars are cool")
+elif loc == 'Bank':
+	print("Money is cool")
+elif loc == "Store":
+	print('Welcome to the store')
+else:
+	print('I do not know much')
+
+
+name = 'Emilio'
+if name == 'Frankie':
+	print('Hello Frankie!')
+elif name == 'Samy':
+	print('Hello Samy')
+else:
+	print('What is your name?')
+
+
+
